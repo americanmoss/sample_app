cddf456492860a90c08aee2141a9b57470391397
Moss Amer <mookman74@gmail.com>
2014-03-23 14:04:15 -0400

    Started testing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+	
   def home
   end
 
   def contact
   end
+
+  def about
+  end
+
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,17 +1,33 @@
 require 'spec_helper'
 
 describe StaticPagesController do
+  render_views
 
   describe "GET 'home'" do
+
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+
+  it "should have the right title" do
+    visit '/static_pages/home'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    
   end
+
+end
 
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
+      response.should be_success
+    end
+  end
+
+    describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
       response.should be_success
     end
   end
