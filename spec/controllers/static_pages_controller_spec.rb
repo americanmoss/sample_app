3ed4ce48024a545f946ba73d04b82d7adf72b1b6
Moss Amer <mookman74@gmail.com>
2014-03-23 18:08:46 -0400

    Slightly dynamic pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -10,26 +10,44 @@ describe StaticPagesController do
       response.should be_success
     end
 
-  it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    
-  end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+
+    it "should have a non-blank body" do
+      visit '/static_pages/home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
 
 end
 
   describe "GET 'contact'" do
+
     it "returns http success" do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+
   end
 
     describe "GET 'about'" do
+
     it "returns http success" do
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+        visit '/static_pages/about'
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+
   end
 
 end
