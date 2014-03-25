110dc5a9878739700ff259b10c9f4ffc64181f07
Moss Amer <mookman74@gmail.com>
2014-03-25 19:38:33 -0400

    Done with chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
   root  'static_pages#home'
   match '/signup', to: "users#new", via: "get"
   match '/contact', to: "static_pages#contact", via: "get"
